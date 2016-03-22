c5c666f0fd4f1cf17501ecd4e20ea373c6cc8387
Pavel Karateev <lancelote.du.lac@gmail.com>
2016-03-22 14:09:56 +0300

    Fixes #2, nonexistent file case

--- fake/html_pages.py
+++ fake/html_pages.py
@@ -22,21 +22,24 @@ class HtmlPagesConverter(object):
         access them
 
         Args:
-            filename (str): Name of the HTML file
+            file_access: FileAccessWrapper
         """
         self.file_access = file_access
         self.breaks = [0]
 
-        with self.file_access.open() as f:
-            while True:
-                line = f.readline()
-                if not line:
-                    break
-                line = line.rstrip()
-                if 'PAGE_BREAK' in line:
-                    page_break_position = f.tell()
-                    self.breaks.append(f.tell())
-            self.breaks.append(f.tell())
+        try:
+            with self.file_access.open() as f:
+                while True:
+                    line = f.readline()
+                    if not line:
+                        break
+                    line = line.rstrip()
+                    if 'PAGE_BREAK' in line:
+                        page_break_position = f.tell()
+                        self.breaks.append(page_break_position)
+                self.breaks.append(f.tell())
+        except FileNotFoundError:
+            pass
 
     def get_html_page(self, page):
         """Return html page with the given number (zero indexed)
@@ -47,6 +50,9 @@ class HtmlPagesConverter(object):
         Returns:
             str: html code
         """
+        if self.breaks == [0]:
+            return ''
+
         page_start = self.breaks[page]
         page_end = self.breaks[page + 1]
         html = ''
